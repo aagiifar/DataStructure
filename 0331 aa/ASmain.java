a8f2ffa0aa4a0d47a43f47642c17ac9378df7713
aagiifar <49718564+aagiifar@users.noreply.github.com>
2021-03-31 16:55:01 +0900

    Add files via upload

---
+++ 0331 aa/ASmain.java
@@ -0,0 +1,21 @@
+package arraystack;
+
+public class ASmain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    ArrayStack<String> stack = new ArrayStack<String>();
+
+    stack.push("apple");
+    stack.push("orange");
+    stack.push("cherry");
+    System.out.println(stack.peek());
+    stack.push("pear");
+    stack.print();
+    stack.pop();
+    System.out.println(stack.peek());
+    stack.push("grape");
+    stack.print();
+	}
+
+}
